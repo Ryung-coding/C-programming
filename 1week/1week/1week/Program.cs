8d1276ebe9febce2594492a41fce663c68c2dbf9
ryung <ryung9514@naver.com>
2025-03-05 15:12:42 +0900

    기본적인 라벨 이용법

---
+++ 1week/1week/1week/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1week
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello mrl");
+            // 콘솔앱의 경우, 절차지향구조에 따라 다음 아래의 Main문을 한번 돌고 끝남
+            // -> 이에 따라 이벤트를 쓰는건 windows Form앱을 쓴다
+        }
+    }
+}
---
+++ 1week/1week_WindowsFormsApp/1week_WindowsFormsApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1week_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        int i = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            i++;
+
+            label1.Text = i.ToString()+"번!!!!";
+
+            //MessageBox.Show("message cmd");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
