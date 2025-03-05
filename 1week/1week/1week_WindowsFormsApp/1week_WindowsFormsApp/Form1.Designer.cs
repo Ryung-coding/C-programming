2c20c6a8d4edc3907d61635550f4383b7cade438
ryung <ryung9514@naver.com>
2025-03-05 15:44:33 +0900

    addOne 과제 + 경로 구조 변경

---
+++ 1week/1week/1week_WindowsFormsApp/1week_WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _1week_WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnReset.Location = new System.Drawing.Point(25, 139);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(92, 48);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblNum.Location = new System.Drawing.Point(22, 27);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(95, 109);
+            this.lblNum.TabIndex = 2;
+            this.lblNum.Text = "0";
+            this.lblNum.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdd.Location = new System.Drawing.Point(24, 193);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 49);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // up
+            // 
+            this.up.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.up.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.up.Location = new System.Drawing.Point(24, 248);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(92, 47);
+            this.up.TabIndex = 4;
+            this.up.Text = "upup~";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 301);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 21);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(139, 335);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.btnReset);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 1week/1week/1week_WindowsFormsApp/1week_WindowsFormsApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1week_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        int total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblNum.Text = "0";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(lblNum.Text);
+            n++;
+            lblNum.Text = Convert.ToString(n);
+        }
+        private void up_Click(object sender, EventArgs e)
+        {
+            total+= Convert.ToInt32(lblNum.Text);
+            textBox1.Text = Convert.ToString(total);
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+    }
+}
