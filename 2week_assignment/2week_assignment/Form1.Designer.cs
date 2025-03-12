1fdbcbf9d39d6475233328b3ffb4053beec2621c
ryung <ryung-coding>
2025-03-12 14:48:38 +0900

    catchMe 과제물

--- 2week_assignment/2week_assignment/Form1.Designer.cs
+++ 2week_assignment/2week_assignment/Form1.Designer.cs
@@ -29,12 +29,71 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label = new System.Windows.Forms.Label();
+            this.print_label = new System.Windows.Forms.Label();
+            this.time_Controller = new System.Windows.Forms.Timer(this.components);
+            this.undo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label.Font = new System.Drawing.Font("한컴 훈민정음 세로쓰기", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label.Location = new System.Drawing.Point(12, 25);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(48, 35);
+            this.label.TabIndex = 0;
+            this.label.Text = "10";
+            // 
+            // print_label
+            // 
+            this.print_label.AutoSize = true;
+            this.print_label.Font = new System.Drawing.Font("한컴 훈민정음 세로쓰기", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.print_label.Location = new System.Drawing.Point(53, 25);
+            this.print_label.Name = "print_label";
+            this.print_label.Size = new System.Drawing.Size(264, 35);
+            this.print_label.TabIndex = 1;
+            this.print_label.Text = "s 남았습니다 ㅋㅋ";
+            // 
+            // time_Controller
+            // 
+            this.time_Controller.Enabled = true;
+            this.time_Controller.Interval = 200;
+            this.time_Controller.Tick += new System.EventHandler(this.time_Controller_Tick);
+            // 
+            // undo
+            // 
+            this.undo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.undo.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.undo.Location = new System.Drawing.Point(127, 108);
+            this.undo.Name = "undo";
+            this.undo.Size = new System.Drawing.Size(43, 23);
+            this.undo.TabIndex = 2;
+            this.undo.Text = "undo";
+            this.undo.UseVisualStyleBackColor = false;
+            this.undo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.undo_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(333, 256);
+            this.Controls.Add(this.undo);
+            this.Controls.Add(this.print_label);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label;
+        protected internal System.Windows.Forms.Label print_label;
+        private System.Windows.Forms.Timer time_Controller;
+        private System.Windows.Forms.Button undo;
     }
 }
 
