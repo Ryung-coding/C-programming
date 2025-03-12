e51aa0716d61842104eb7d53d3647837cff9cc8b
ryung <ryung-coding>
2025-03-12 15:26:53 +0900

    draw 과제물

--- 2week_assignment/2week_assignment/Form1.Designer.cs
+++ 2week_assignment/2week_assignment/Form1.Designer.cs
@@ -28,22 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.label = new System.Windows.Forms.Label();
             this.print_label = new System.Windows.Forms.Label();
-            this.time_Controller = new System.Windows.Forms.Timer(this.components);
-            this.undo = new System.Windows.Forms.Button();
+            this.pic_draw = new System.Windows.Forms.PictureBox();
+            this.green = new System.Windows.Forms.RadioButton();
+            this.blue = new System.Windows.Forms.RadioButton();
+            this.red = new System.Windows.Forms.RadioButton();
+            this.color_box = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_draw)).BeginInit();
+            this.color_box.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label
-            // 
-            this.label.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label.Font = new System.Drawing.Font("한컴 훈민정음 세로쓰기", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label.Location = new System.Drawing.Point(12, 25);
-            this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(48, 35);
-            this.label.TabIndex = 0;
-            this.label.Text = "10";
             // 
             // print_label
             // 
@@ -51,49 +44,90 @@
             this.print_label.Font = new System.Drawing.Font("한컴 훈민정음 세로쓰기", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.print_label.Location = new System.Drawing.Point(53, 25);
             this.print_label.Name = "print_label";
-            this.print_label.Size = new System.Drawing.Size(264, 35);
+            this.print_label.Size = new System.Drawing.Size(0, 35);
             this.print_label.TabIndex = 1;
-            this.print_label.Text = "s 남았습니다 ㅋㅋ";
             // 
-            // time_Controller
+            // pic_draw
             // 
-            this.time_Controller.Enabled = true;
-            this.time_Controller.Interval = 200;
-            this.time_Controller.Tick += new System.EventHandler(this.time_Controller_Tick);
+            this.pic_draw.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pic_draw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_draw.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pic_draw.Location = new System.Drawing.Point(12, 12);
+            this.pic_draw.Name = "pic_draw";
+            this.pic_draw.Size = new System.Drawing.Size(475, 520);
+            this.pic_draw.TabIndex = 2;
+            this.pic_draw.TabStop = false;
+            this.pic_draw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_draw_MouseDown);
+            this.pic_draw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_draw_MouseMove);
             // 
-            // undo
+            // green
             // 
-            this.undo.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.undo.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.undo.Location = new System.Drawing.Point(127, 108);
-            this.undo.Name = "undo";
-            this.undo.Size = new System.Drawing.Size(43, 23);
-            this.undo.TabIndex = 2;
-            this.undo.Text = "undo";
-            this.undo.UseVisualStyleBackColor = false;
-            this.undo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.undo_MouseMove);
+            this.green.AutoSize = true;
+            this.green.Location = new System.Drawing.Point(6, 40);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(55, 16);
+            this.green.TabIndex = 4;
+            this.green.Text = "green";
+            this.green.UseVisualStyleBackColor = true;
+            // 
+            // blue
+            // 
+            this.blue.AutoSize = true;
+            this.blue.Location = new System.Drawing.Point(6, 64);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(47, 16);
+            this.blue.TabIndex = 5;
+            this.blue.Text = "blue";
+            this.blue.UseVisualStyleBackColor = true;
+            // 
+            // red
+            // 
+            this.red.AutoSize = true;
+            this.red.Location = new System.Drawing.Point(6, 18);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(41, 16);
+            this.red.TabIndex = 6;
+            this.red.Text = "red";
+            this.red.UseVisualStyleBackColor = true;
+            // 
+            // color_box
+            // 
+            this.color_box.Controls.Add(this.green);
+            this.color_box.Controls.Add(this.blue);
+            this.color_box.Controls.Add(this.red);
+            this.color_box.Location = new System.Drawing.Point(493, 12);
+            this.color_box.Name = "color_box";
+            this.color_box.Size = new System.Drawing.Size(81, 88);
+            this.color_box.TabIndex = 7;
+            this.color_box.TabStop = false;
+            this.color_box.Text = "color";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 256);
-            this.Controls.Add(this.undo);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(583, 544);
+            this.Controls.Add(this.color_box);
+            this.Controls.Add(this.pic_draw);
             this.Controls.Add(this.print_label);
-            this.Controls.Add(this.label);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "windows";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_draw)).EndInit();
+            this.color_box.ResumeLayout(false);
+            this.color_box.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label;
         protected internal System.Windows.Forms.Label print_label;
-        private System.Windows.Forms.Timer time_Controller;
-        private System.Windows.Forms.Button undo;
+        private System.Windows.Forms.PictureBox pic_draw;
+        private System.Windows.Forms.RadioButton green;
+        private System.Windows.Forms.RadioButton blue;
+        private System.Windows.Forms.RadioButton red;
+        private System.Windows.Forms.GroupBox color_box;
     }
 }
 
--- 2week_assignment/2week_assignment/Form1.cs
+++ 2week_assignment/2week_assignment/Form1.cs
@@ -12,52 +12,38 @@ namespace _2week_assignment
 {
     public partial class Form1 : Form
     {
-        Random rnd = new Random(); //Heap에다가 참조변수를 저장해서 rnd라는 이름으로 
-        // Random : 클래스 -> .NET에서 제공하는 기본 클래스
-        // rnd : 이건 참조변수! -> 이건 참조형식으로 정의해야한다 1. Array 2. Class 3. String
-        // new Random() : 클래스의 객체에 메모리를 할당해서 참조를 반환
-        // 이 공간은 프로그램이 실행될때, 작동되는 구조이다.
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void time_Controller_Tick(object sender, EventArgs e)
+        int x_past = 0; int y_past = 0;
+
+
+        private void pic_draw_MouseMove(object sender, MouseEventArgs e)
         {
-            //도구상자내 [구성요소] -> [타이머]
+            if (e.Button != MouseButtons.Left) return;
+            Graphics grp = pic_draw.CreateGraphics();
+            
+            Color col = Color.Brown;
+            
+            if (red.Checked) col = Color.Red;
+            if (green.Checked) col = Color.Green;
+            if (blue.Checked) col = Color.Blue;
 
-            // Enabled를 true로 하면 실행시 바로 작동
-            // interval : 몇 ms로 돌것인지 설정
-            // 타이머의 [속성]안 [번개]를 누르면 Tick이라는 이벤트 존재
-
-            //int count = (int)label.Text; -> 이거 안된다! string to int, int to string
-            int count = Convert.ToInt32(label.Text);
-            count--;
-            label.Text = Convert.ToString(count);
-            //label.Text = count.ToString();
-            if (count == 0)
-            {
-                time_Controller.Enabled = false; //break 방식
-                MessageBox.Show("lol :) Kikikikiki"); //메세지 형이 나오는 구문, 버튼 눌러야 다음으로 진행
-                Application.Exit(); //프로그램이 끝남
-            }
-
+            Pen pen = new Pen(col);
+            // grp.DrawEllipse(pen, e.X, e.Y, 10, 10); -> 이러면 너무 이산적으로 그려진다 -> 그전과 이전을 선으로 이어보자
+            grp.DrawLine(pen, x_past, y_past, e.X, e.Y); 
+            x_past = e.X;
+            y_past = e.Y;
         }
 
-        private void undo_MouseMove(object sender, MouseEventArgs e)
+        private void pic_draw_MouseDown(object sender, MouseEventArgs e)
         {
-            // 버튼의 [속성]안 [번개] 내 [클릭]이 디폴트 이벤트
-            // MouseMove : 버튼위에 마우스가 움직이면 작동됨
-            // 버튼명.Left는 버튼의 좌측 하단기준으로 윈도우 왼쪽까지의 px량
-            // 버튼명.Top는 버튼의 상단 기준으로 윈도우 위쪽까지의 px량
-            // Note* 윈도우의 좌측상단이 (0,0)이다
-
-            undo.Left = rnd.Next(this.ClientSize.Width-undo.Width); 
-            // rnd라는 객체의 Next라는 함수를 써서 (N)을 쓰면 0~N-1이 랜덤으로 나옴
-            undo.Top = rnd.Next(this.ClientSize.Height - undo.Height); 
-            // 이때 this라는 것은 내가 지금 Form이라는 내에서 Form의 값을 쓰고 있어서 this. 을 통해 구해온다 나머진 괜찮다
-
+            // 이런식으로 해야 시작 초기값에 대한 튐을 방지한다
+            // 이러면 초기값이 마우스를 누른 순간으로 되니 점프하는일이 없다
+            x_past = e.X; 
+            y_past = e.Y;
         }
     }
 }
