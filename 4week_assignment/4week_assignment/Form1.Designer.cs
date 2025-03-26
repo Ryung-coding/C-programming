5bb5c25e1297661c8c0df7467b8c061ee7557560
ryung <ryung9514@naver.com>
2025-03-26 14:44:21 +0900

    카드 뒤집기 게임

--- 4week_assignment/4week_assignment/Form1.Designer.cs
+++ 4week_assignment/4week_assignment/Form1.Designer.cs
@@ -29,12 +29,133 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.count = new System.Windows.Forms.Label();
+            this.card01 = new System.Windows.Forms.PictureBox();
+            this.card03 = new System.Windows.Forms.PictureBox();
+            this.card04 = new System.Windows.Forms.PictureBox();
+            this.card02 = new System.Windows.Forms.PictureBox();
+            this.card00 = new System.Windows.Forms.PictureBox();
+            this.timer_open = new System.Windows.Forms.Timer(this.components);
+            this.timer_closed = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.card01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card00)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // count
+            // 
+            this.count.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.count.AutoSize = true;
+            this.count.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.count.Location = new System.Drawing.Point(89, 226);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(64, 73);
+            this.count.TabIndex = 0;
+            this.count.Text = "0";
+            // 
+            // card01
+            // 
+            this.card01.Image = global::_4week_assignment.Properties.Resources.closed;
+            this.card01.Location = new System.Drawing.Point(12, 114);
+            this.card01.Name = "card01";
+            this.card01.Size = new System.Drawing.Size(71, 96);
+            this.card01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.card01.TabIndex = 1;
+            this.card01.TabStop = false;
+            this.card01.Click += new System.EventHandler(this.card_Click);
+            // 
+            // card03
+            // 
+            this.card03.Image = global::_4week_assignment.Properties.Resources.closed;
+            this.card03.Location = new System.Drawing.Point(12, 318);
+            this.card03.Name = "card03";
+            this.card03.Size = new System.Drawing.Size(71, 96);
+            this.card03.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.card03.TabIndex = 2;
+            this.card03.TabStop = false;
+            this.card03.Click += new System.EventHandler(this.card_Click);
+            // 
+            // card04
+            // 
+            this.card04.Image = global::_4week_assignment.Properties.Resources.closed;
+            this.card04.Location = new System.Drawing.Point(12, 420);
+            this.card04.Name = "card04";
+            this.card04.Size = new System.Drawing.Size(71, 96);
+            this.card04.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.card04.TabIndex = 3;
+            this.card04.TabStop = false;
+            this.card04.Click += new System.EventHandler(this.card_Click);
+            // 
+            // card02
+            // 
+            this.card02.Image = global::_4week_assignment.Properties.Resources.closed;
+            this.card02.Location = new System.Drawing.Point(12, 216);
+            this.card02.Name = "card02";
+            this.card02.Size = new System.Drawing.Size(71, 96);
+            this.card02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.card02.TabIndex = 4;
+            this.card02.TabStop = false;
+            this.card02.Click += new System.EventHandler(this.card_Click);
+            // 
+            // card00
+            // 
+            this.card00.Image = global::_4week_assignment.Properties.Resources.closed;
+            this.card00.Location = new System.Drawing.Point(12, 12);
+            this.card00.Name = "card00";
+            this.card00.Size = new System.Drawing.Size(71, 96);
+            this.card00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.card00.TabIndex = 5;
+            this.card00.TabStop = false;
+            this.card00.Click += new System.EventHandler(this.card_Click);
+            // 
+            // timer_open
+            // 
+            this.timer_open.Enabled = true;
+            this.timer_open.Interval = 500;
+            this.timer_open.Tick += new System.EventHandler(this.timer_open_Tick);
+            // 
+            // timer_closed
+            // 
+            this.timer_closed.Interval = 200;
+            this.timer_closed.Tick += new System.EventHandler(this.timer_closed_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(163, 528);
+            this.Controls.Add(this.card00);
+            this.Controls.Add(this.card02);
+            this.Controls.Add(this.card04);
+            this.Controls.Add(this.card03);
+            this.Controls.Add(this.card01);
+            this.Controls.Add(this.count);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.card01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card00)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.PictureBox card01;
+        private System.Windows.Forms.PictureBox card03;
+        private System.Windows.Forms.PictureBox card04;
+        private System.Windows.Forms.PictureBox card02;
+        private System.Windows.Forms.PictureBox card00;
+        private System.Windows.Forms.Timer timer_open;
+        private System.Windows.Forms.Timer timer_closed;
     }
 }
 
--- 4week_assignment/4week_assignment/Form1.cs
+++ 4week_assignment/4week_assignment/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,71 @@ namespace _4week_assignment
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random(); //Random class 정의
+        bool[] isopen = new bool[5] { false, false, false, false, false };
+
+        private void closed_all()
+        {
+            card00.Image = Properties.Resources.closed;
+            card01.Image = Properties.Resources.closed;
+            card02.Image = Properties.Resources.closed;
+            card03.Image = Properties.Resources.closed;
+            card04.Image = Properties.Resources.closed;
+            timer_open.Enabled = true;
+            timer_closed.Enabled = false;
+            for (int i = 0; i < 5; i++) isopen[i] = false;
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer_open_Tick(object sender, EventArgs e) //timer가 open이 된 순간 
+        {
+            int random_number = rnd.Next(5);
+            switch (random_number)
+            {
+                case 0:
+                    card00.Image = Properties.Resources.open;
+                    break;
+
+                case 1:
+                    card01.Image = Properties.Resources.open;
+                    break;
+
+                case 2:
+                    card02.Image = Properties.Resources.open;
+                    break;
+
+                case 3:
+                    card03.Image = Properties.Resources.open;
+                    break;
+
+                case 4:
+                    card04.Image = Properties.Resources.open;
+                    break;
+            }
+            timer_open.Enabled = false;
+            timer_closed.Enabled = true;
+            isopen[random_number] = true;
+        }
+
+        private void timer_closed_Tick(object sender, EventArgs e)
+        {
+            closed_all();
+        }
+
+        private void card_Click(object sender, EventArgs e) //sender가 열렸는지에 대한 이벤트를 알려줌
+        {
+            int Hit_card = 0;
+            PictureBox obj2pic = (PictureBox)sender;
+            if (obj2pic == card00) Hit_card = 0;
+            if (obj2pic == card01) Hit_card = 1;
+            if (obj2pic == card02) Hit_card = 2;
+            if (obj2pic == card03) Hit_card = 3;
+            if (obj2pic == card04) Hit_card = 4;
+            count.Text = (isopen[Hit_card] ? 1 + Convert.ToInt32(count.Text) : Convert.ToInt32(count.Text)).ToString();
+            closed_all();
         }
     }
 }
