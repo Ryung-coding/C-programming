131f0cd7bc04946ee37d3ee76746538d1ab5c162
ryung <ryung9514@naver.com>
2025-04-02 15:53:52 +0900

    몬데타를로

--- 5week_assignment/5week_assignment/Form1.Designer.cs
+++ 5week_assignment/5week_assignment/Form1.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.start = new System.Windows.Forms.Button();
+            this.picArea = new System.Windows.Forms.PictureBox();
+            this.ratio_real = new System.Windows.Forms.Label();
+            this.in_area = new System.Windows.Forms.Label();
+            this.out_area = new System.Windows.Forms.Label();
+            this.ratio_monte = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picArea)).BeginInit();
             this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(368, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(101, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // picArea
+            // 
+            this.picArea.BackColor = System.Drawing.Color.White;
+            this.picArea.Location = new System.Drawing.Point(12, 12);
+            this.picArea.Name = "picArea";
+            this.picArea.Size = new System.Drawing.Size(348, 263);
+            this.picArea.TabIndex = 1;
+            this.picArea.TabStop = false;
+            // 
+            // ratio_real
+            // 
+            this.ratio_real.AutoSize = true;
+            this.ratio_real.Location = new System.Drawing.Point(366, 43);
+            this.ratio_real.Name = "ratio_real";
+            this.ratio_real.Size = new System.Drawing.Size(56, 12);
+            this.ratio_real.TabIndex = 2;
+            this.ratio_real.Text = "ratio_real";
+            // 
+            // in_area
+            // 
+            this.in_area.AutoSize = true;
+            this.in_area.Location = new System.Drawing.Point(366, 66);
+            this.in_area.Name = "in_area";
+            this.in_area.Size = new System.Drawing.Size(46, 12);
+            this.in_area.TabIndex = 3;
+            this.in_area.Text = "in_area";
+            // 
+            // out_area
+            // 
+            this.out_area.AutoSize = true;
+            this.out_area.Location = new System.Drawing.Point(366, 89);
+            this.out_area.Name = "out_area";
+            this.out_area.Size = new System.Drawing.Size(53, 12);
+            this.out_area.TabIndex = 4;
+            this.out_area.Text = "out_area";
+            // 
+            // ratio_monte
+            // 
+            this.ratio_monte.AutoSize = true;
+            this.ratio_monte.Location = new System.Drawing.Point(366, 112);
+            this.ratio_monte.Name = "ratio_monte";
+            this.ratio_monte.Size = new System.Drawing.Size(70, 12);
+            this.ratio_monte.TabIndex = 5;
+            this.ratio_monte.Text = "ratio_monte";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(260, 232);
+            this.ClientSize = new System.Drawing.Size(481, 291);
+            this.Controls.Add(this.ratio_monte);
+            this.Controls.Add(this.out_area);
+            this.Controls.Add(this.in_area);
+            this.Controls.Add(this.ratio_real);
+            this.Controls.Add(this.picArea);
+            this.Controls.Add(this.start);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picArea)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox picArea;
+        private System.Windows.Forms.Label ratio_real;
+        private System.Windows.Forms.Label in_area;
+        private System.Windows.Forms.Label out_area;
+        private System.Windows.Forms.Label ratio_monte;
     }
 }
 
--- 5week_assignment/5week_assignment/Form1.cs
+++ 5week_assignment/5week_assignment/Form1.cs
@@ -12,9 +12,49 @@ namespace _5week_assignment
 {
     public partial class Form1 : Form
     {
+        const int N = 50000;
+        Random rnd = new Random();
+
+        bool InReact(int x_min, int x_max, int y_min, int y_max, int x_point, int y_point)
+        {
+            if (x_point < x_min || x_point > x_max) return false;
+            if (y_point < y_min || y_point > y_max) return false;
+            return true;
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            int width = picArea.ClientSize.Width;
+            int height = picArea.ClientSize.Height;
+            int area = width * height;
+
+            ratio_real.Text = Convert.ToString((100.0 * 100.0) / (double)area);
+
+            Graphics grp = picArea.CreateGraphics();
+            int In_count = 0, Out_count = 0;
+
+
+            for (int step=0; step < N; step++)
+            {
+                int x_point = rnd.Next(width);
+                int y_point = rnd.Next(height);
+
+                if(InReact(100,100,199,199, x_point,y_point) == true)
+                {
+                    In_count++;
+                    grp.DrawEllipse(new Pen(Color.Black), x_point, y_point, 1, 1);
+                }
+                else
+                {
+                    Out_count++;
+                    grp.DrawEllipse(new Pen(Color.Silver), x_point, y_point, 1, 1);
+                }
+                ratio_monte.Text = Convert.ToString((double)In_count / (double)Out_count);
+            }
         }
     }
 }
