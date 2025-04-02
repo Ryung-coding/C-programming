7f639e1955638fcc50d72d2d8e3777c88180d31e
ryung <ryung9514@naver.com>
2025-04-02 14:11:19 +0900

    5week assignment build

---
+++ 5week_assignment/StringHandling_Frame/StringHandling/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime t = DateTime.Now; //Now뒤에 ()가 없기에 속성, 있다면 메소드 DateTime은 클래스다.
+
+        }
+    }
+}
