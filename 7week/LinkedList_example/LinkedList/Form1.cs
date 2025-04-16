83673351367d5026de65ac7877907b1f18841cfa
ryung <ryung9514@naver.com>
2025-04-16 15:50:50 +0900

    remove함수까지 완료

---
+++ 7week/LinkedList_example/LinkedList/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace LinkedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleList simplelist = new SimpleList();
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            Node node;
+            node = new Node(0); simplelist.Add_at_header(node);
+            node = new Node(1); simplelist.Add_at_header(node);
+            node = new Node(2); simplelist.Add_at_header(node);
+            txtPrint.Text = simplelist.Print();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            simplelist.Clear();
+            txtPrint.Text = simplelist.Print();
+        }
+
+        private void btnMake20_Click(object sender, EventArgs e)
+        {
+            for(int i=0; i<20;i++)
+            {
+                // Node node = new Node(i);
+                // simplelist.Append(node);
+                new Node(i, simplelist); 
+                // 메모리가 잡히면서 내부 클래스에서 참조변수가 따로 존재하므로 가능
+            }
+            txtPrint.Text = simplelist.Print();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            Node removed_node = simplelist.FindNodeOfData(Convert.ToInt32(txtRemoveData.Text));
+            simplelist.Remove(removed_node);
+            txtPrint.Text = simplelist.Print();
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            //simplelist = simplelist.Reverse();
+            txtPrint.Text = simplelist.Print();
+        }
+    }
+}
---
+++ 7week/LinkedList_example/LinkedList/SimpleList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+   class Node //노드를 활용해서 Linked List를 구현하는 클래스
+    {
+        public int Data; //data가 담겨있는 곳
+        public Node Next; //Next point에 대한 참조위치다
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        public Node(int data, SimpleList list)
+        {
+            Data = data;
+            Next = null;
+            list.Append(this); //this를 통해서 내가 생성될떄 바로 들어가지도록 함.
+            //this가 
+        }
+
+
+    }
+    class SimpleList
+    {
+        private Node head = null; //딱히 권한에 대한 것을 안적었기에 private이다.
+        public string Print()
+        {
+            if (head == null) return "Empty\r\n";
+            string st = "";
+            for (Node n = head; n != null; n = n.Next)
+            {
+                st += n.Data.ToString() + "\r\n";
+            }
+            st += "-END-\r\n";
+            return st;
+        }
+        public void Add_at_header(Node node)
+        {
+            node.Next = head;
+            head = node;
+        }
+
+        public void Clear()
+        {
+            head = null;
+        }
+
+        public void Append(Node node)
+        {
+            if (head == null)
+            {
+                node.Next = null;
+                head = node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.Next = node;
+                node.Next = null;
+            }
+        }
+
+        private Node GetLastNode()
+        {
+            if (head == null)
+                return null;
+            else
+            {
+                Node node = head;
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+                return node;
+            }
+        }
+
+        public void Remove(Node n)
+        {
+            for (Node before_node = head; before_node != null; before_node = before_node.Next)
+            {
+                if (before_node.Next == n) before_node.Next = n.Next;
+            }
+            n.Next = null;
+        }
+        public Node FindNodeOfData(int data)
+        {
+            for (Node n = head; n != null; n = n.Next)
+            {
+                if (n.Data == data) return n;
+            }
+            return null;
+        }
+
+        //public SimpleList Reverse()
+        //{
+
+        //}
+
+    }
+}
