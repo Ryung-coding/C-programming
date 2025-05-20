29ae9bb2fc5da62f87eae4ee353bca07d65756e0
ryung <ryung9514@naver.com>
2025-05-20 17:34:09 +0900

    기본코드 저장

---
+++ 12week/4_Server_Client/tClient/tClient/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace tClient
+{
+    public partial class Form1 : Form
+    {
+        private TClient clientChat; //채팅용
+        private TClient clientCopy; //원위치복사용
+        private TClient clientComm; //비트통신용
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string hostname = TSocket.HostName();
+            IPAddress[] addrs = TSocket.HostAddresses();
+
+            string st = hostname + "\r\n";
+            for (int i = 0; i < addrs.Length; i++)
+            {
+                st = st + addrs[i].ToString() + "\r\n";
+            }
+
+            lblComInfo.Text = st;
+
+            btnServerMe.PerformClick();
+        }
+
+        private void btnServerMe_Click(object sender, EventArgs e)
+        {
+            txtServerIP.Text = TSocket.HostAddressesv4().ToString();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (clientChat != null) clientChat.ClientClose();
+            if (clientCopy != null) clientCopy.ClientClose();
+            if (clientComm != null) clientComm.ClientClose();
+        }
+
+        private void timConnStatus_Tick(object sender, EventArgs e)
+        {
+            if (clientChat == null) { lblConnChat.Text = "Chat : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = clientChat.ClientStatus();
+                lblConnChat.Text = "Chat : " + conn.ToString();
+            }
+
+            if (clientCopy == null) { lblConnCopy.Text = "Copy : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = clientCopy.ClientStatus();
+                lblConnCopy.Text = "Copy : " + conn.ToString();
+            }
+
+            if (clientComm == null) { lblConnComm.Text = "Comm : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = clientComm.ClientStatus();
+                lblConnComm.Text = "Comm : " + conn.ToString();
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string serverIP = txtServerIP.Text; 
+            string clientIP = TSocket.HostAddressesv4().ToString();
+
+            if (clientChat == null) clientChat = new TClient();
+            clientChat.ClientBeginConnect(serverIP, 5000, clientIP);   // 1024~65535 추천
+
+            if (clientCopy == null) clientCopy = new TClient();
+            clientCopy.ClientBeginConnect(serverIP, 5001, clientIP);   // 1024~65535 추천
+
+            if (clientComm == null) clientComm = new TClient();
+            clientComm.ClientBeginConnect(serverIP, 5002, clientIP);   // 1024~65535 추천
+        }
+    }
+}
---
+++ 12week/4_Server_Client/tServer/tServer/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace tServer
+{
+    public partial class Form1 : Form
+    {
+        private TServer serverChat; //채팅용
+        private TServer serverCopy; //원위치복사용
+        private TServer serverComm; //비트통신용
+
+        private string rbuffcir = "";   // 원위치송신메시지 저장버퍼
+        private string rbuffbit = "";   // 비트정보 asking 메시지 저장버퍼
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            string hostname = TSocket.HostName();
+            IPAddress[] addrs = TSocket.HostAddresses();
+
+            string st = hostname + "\r\n";
+            for (int i = 0; i < addrs.Length; i++)
+            {
+                st = st + addrs[i].ToString() + "\r\n";
+            }
+
+            lblComInfo.Text = st;
+
+            btnServerMe.PerformClick();
+        }
+
+        private void btnServerMe_Click(object sender, System.EventArgs e)
+        {
+            txtMyIP.Text = TSocket.HostAddressesv4().ToString();
+        }
+
+        private void btnClose_Click(object sender, System.EventArgs e)
+        {
+            if (serverChat != null) serverChat.ServerClose();
+            if (serverCopy != null) serverCopy.ServerClose();
+            if (serverComm != null) serverComm.ServerClose();
+        }
+
+        private void timConnStatus_Tick(object sender, System.EventArgs e)
+        {
+            if (serverChat == null) { lblConnChat.Text = "Chat : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = serverChat.ServerStatus();
+                lblConnChat.Text = "Chat : " + conn.ToString();
+            }
+
+            if (serverCopy == null) { lblConnCopy.Text = "Copy : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = serverCopy.ServerStatus();
+                lblConnCopy.Text = "Copy : " + conn.ToString();
+            }
+
+            if (serverComm == null) { lblConnComm.Text = "Comm : " + "NULL"; }
+            else
+            {
+                csConnStatus conn = serverComm.ServerStatus();
+                lblConnComm.Text = "Comm : " + conn.ToString();
+            }
+        }
+
+        private void btnListen_Click(object sender, System.EventArgs e)
+        {
+            string myIP = txtMyIP.Text;
+
+            if (serverChat == null) serverChat = new TServer();
+            serverChat.ServerStartListen(myIP, 5000);   // 1024~65535 추천
+
+            if (serverCopy == null) serverCopy = new TServer(CirclePosDataArrived);
+            serverCopy.ServerStartListen(myIP, 5001);   // 1024~65535 추천
+
+            if (serverComm == null) serverComm = new TServer(AskingBitsDataArrived);
+            serverComm.ServerStartListen(myIP, 5002);   // 1024~65535 추천
+        }
+        private void CirclePosDataArrived()
+        {
+
+        }
+
+        private void AskingBitsDataArrived()
+        {
+
+        }
+
+    }
+}
